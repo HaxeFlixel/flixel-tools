e1e27c008de65ff9d1814b472d090901130000b3
impaler <user123@server.fake>
2013-07-21 15:35:53 +0930

    testing the setup command prompt

--- src/commands/SetupCommand.hx
+++ src/commands/SetupCommand.hx
@@ -1,5 +1,7 @@
 package commands;
 
+import sys.FileSystem;
+import utils.CommandUtils.FlxToolSettings;
 import utils.CommandUtils;
 import sys.io.File;
 import massive.sys.io.FileSys;
@@ -7,7 +9,6 @@ import massive.sys.cmd.Command;
 
 class SetupCommand extends Command
 {
-
 	private var autoContinue:Bool = false;
 
 	override public function execute():Void
@@ -22,6 +23,8 @@ class SetupCommand extends Command
 
 		var haxePath:String = Sys.getEnv("HAXEPATH");
 
+		var scriptDestination = "";
+
 		if (FileSys.isWindows)
 		{
 			if (haxePath == null || haxePath == "")
@@ -29,7 +32,12 @@ class SetupCommand extends Command
 				haxePath = "C:\\HaxeToolkit\\haxe\\";
 			}
 
-			File.copy(CommandUtils.getHaxelibPath("flixel-tools") + "\\\\bin\\flixel.bat", haxePath + "\\flixel.bat");
+			scriptDestination = haxePath + "\\flixel.bat";
+
+			if(!FileSystem.exists (scriptDestination))
+			{
+				File.copy(CommandUtils.getHaxelibPath("flixel-tools") + "\\\\bin\\flixel.bat", scriptDestination);
+			}
 		}
 		else
 		{
@@ -38,34 +46,128 @@ class SetupCommand extends Command
 				haxePath = "/usr/lib/haxe";
 			}
 
-			Sys.command("sudo", [ "cp", CommandUtils.getHaxelibPath("flixel-tools") + "bin/flixel.sh", haxePath + "/flixel" ]);
-			Sys.command("sudo chmod 755 " + haxePath + "/flixel");
-			Sys.command("sudo ln -s " + haxePath + "/flixel /usr/bin/flixel");
+			scriptDestination = CommandUtils.getHaxelibPath("flixel-tools") + "bin/flixel.sh";
+
+			if(!FileSystem.exists (scriptDestination))
+			{
+				Sys.command("sudo", [ "cp", scriptDestination, haxePath + "/flixel" ]);
+				Sys.command("sudo chmod 755 " + haxePath + "/flixel");
+				Sys.command("sudo ln -s " + haxePath + "/flixel /usr/bin/flixel");
+			}
 		}
 
-		Sys.println("You have now setup HaxeFlixel");
+		var templatesHaxelib = CommandUtils.getHaxelibPath("flixel-templates");
+		var demosHaxelib = CommandUtils.getHaxelibPath("flixel-demos");
 
-		Sys.command("flixel");
-
-		if(!autoContinue)
+		if(templatesHaxelib == "" || demosHaxelib == "")
 		{
-			var download = CommandUtils.askYN ("Would you now like this tool to download the flixel-demos and
+			if(!autoContinue)
+			{
+				var download = CommandUtils.askYN ("Would you now like this tool to download the flixel-demos and
 		flixel-templates?");
 
-			if(download == Answer.Yes)
-			{
-				Sys.command ("flixel download");
+				if(download == Answer.Yes)
+				{
+					Sys.command ("flixel download");
+				}
 			}
 			else
 			{
-				exit();
+				Sys.command ("flixel download");
 			}
 		}
-		else
+
+		promptForSettings();
+
+		Sys.println("");
+		Sys.println("");
+		Sys.println(" You have now setup HaxeFlixel Tools");
+		Sys.println("");
+		Sys.println(" Try the 'flixel' command to test it :)");
+		Sys.println("");
+		Sys.println(FlxTools.settings.DefaultEditor);
+
+		exit();
+	}
+
+	private function promptForSettings():FlxToolSettings
+	{
+
+		var IDES:Array<String> = [FlxTools.SUBLIME_TEXT, FlxTools.FLASH_DEVELOP, FlxTools.INTELLIJ_IDEA, FlxTools.IDE_NONE];
+		var IDE = FlxTools.IDE_NONE;
+		var AuthorName = "";
+		var sublimeCMD = false;
+		var ideaFlexSDKName = "flex_4.6";
+		var ideaFlixelEngine = "Flixel Engine";
+		var ideaFlixelAddons = "Flixel Addons";
+
+		AuthorName = CommandUtils.askString("Enter the author name to use when generating templates.\n\nJust hit enter to not use an author name.");
+
+		IDE = CommandUtils.askQuestionStrings("Choose your default IDE.", "" , IDES, false);
+		if(IDE == null)
 		{
-			Sys.command ("flixel download");
-			exit();
+			Sys.println(" Your IDE choice was not recognised, using default of " + FlxTools.IDE_NONE);
+			IDE = FlxTools.IDE_NONE;
 		}
 
+		if(IDE == FlxTools.SUBLIME_TEXT)
+		{
+			var name = CommandUtils.askYN("Do you want to open demos/templates automatically with subl command tool?");
+
+			if(name == Answer.Yes)
+				sublimeCMD = true;
+		}
+		else if ( IDE == FlxTools.INTELLIJ_IDEA)
+		{
+			var answer = CommandUtils.askString("Enter the name of your default FlexSDK");
+			if(answer != "")
+				ideaFlexSDKName = answer;
+
+			var answer = CommandUtils.askString("Enter the name of your default Flixel Library, just ENTER for default " + ideaFlixelEngine);
+			if(answer != "")
+				ideaFlixelEngine = answer;
+
+			var answer = CommandUtils.askString("Enter the name of your default Flixel Addons Library, just ENTER for default " + ideaFlixelAddons);
+			if(answer != "")
+				ideaFlixelAddons = answer;
+		}
+		else if (IDE == FlxTools.FLASH_DEVELOP)
+		{
+			//todo execute template zip?
+		}
+
+		var settingsFile:FlxToolSettings =
+		{
+			DefaultEditor:IDE,
+			AuthorName:AuthorName,
+			SublimeCMDOpen:sublimeCMD,
+			IDEA_flexSdkName:ideaFlexSDKName,
+			IDEA_Flixel_Engine_Library:ideaFlixelEngine,
+			IDEA_Flixel_Addons_Library:ideaFlixelAddons,
+		};
+
+		CommandUtils.saveToolSettings(settingsFile);
+
+		Sys.println("");
+		Sys.println(" Your current settings:");
+		Sys.println("");
+
+		Sys.println(" Default Editor			:" + FlxTools.settings.DefaultEditor);
+		Sys.println(" Author Name			:" + FlxTools.settings.AuthorName);
+
+		if(IDE == FlxTools.SUBLIME_TEXT)
+		{
+			Sys.println(" Sublime CMD Option		:" + FlxTools.settings.SublimeCMDOpen);
+		}
+		else if ( IDE == FlxTools.INTELLIJ_IDEA)
+		{
+			Sys.println(" IDEA_flexSdkName		:" + FlxTools.settings.IDEA_flexSdkName);
+			Sys.println(" IDEA_Flixel_Addons_Library	:" + FlxTools.settings.IDEA_Flixel_Addons_Library);
+			Sys.println(" IDEA_Flixel_Engine_Library	:" + FlxTools.settings.IDEA_Flixel_Engine_Library);
+		}
+
+		Sys.println("");
+
+		return settingsFile;
 	}
 }
