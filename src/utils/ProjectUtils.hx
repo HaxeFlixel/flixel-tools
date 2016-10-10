HaxeFlixel/flixel-tools

commit 0e8c67d510ad7fb662293f87cd224e7a0e5de960

configure: support APPLICATION_FILE replacement
see HaxeFlixel/flixel-templates@5c09f25

src/utils/ProjectUtils.hx
@@ -1,5 +1,8 @@
 package utils;
 
+import haxe.io.Path;
+import haxe.xml.Fast;
+import sys.io.File;
 import massive.sys.io.FileSys;
 import utils.CommandUtils;
 import utils.TemplateUtils;
@@ -30,35 +33,38 @@ class ProjectUtils
 	/**
 	 * Recursively search a directory for Lime projects
 	 */
-	public static function findLimeProjects(targetDirectory:String):Array<LimeProject>
+	public static function findLimeProjects(directory:String):Array<LimeProject>
 	{
 		var projects = [];
 
-		if (!FileSys.exists(targetDirectory))
-			return [];
+		if (!FileSys.exists(directory))
+			return projects;
 
-		for (name in FileSys.readDirectory(targetDirectory))
+		var projectPath = findProjectXml(directory);
+		if (projectPath != null)
 		{
-			var folderPath:String = CommandUtils.combine(targetDirectory, name);
-
+			projects.push({
+				name : new Path(projectPath).file,
+				path : directory,
+				projectXmlPath : projectPath,
+				targets : ""
+			});
+		}
+		
+		for (name in FileSys.readDirectory(directory))
+		{
+			var folderPath:String = CommandUtils.combine(directory, name);
 			if (FileSys.isDirectory(folderPath) && !name.startsWith("."))
-			{
-				var projectPath = findProjectXml(folderPath);
-				if (projectPath != null)
-				{
-					projects.push({
-						name : name,
-						path : folderPath,
-						projectXmlPath : projectPath,
-						targets : ""
-					});
-				}
-
 				projects = projects.concat(findLimeProjects(folderPath));
-			}
 		}
 
 		return projects;
+	}
+
+	public static function getApplicationFile(projectXmlPath:String):String
+	{
+		var fast = new Fast(Xml.parse(File.getContent(projectXmlPath)));
+		return fast.node.project.node.app.att.file;
 	}
 
 	/**
@@ -94,9 +100,8 @@ class ProjectUtils
 		}
 
 		var settings = FlxTools.settings;
-		var addOption = function(name:String, defaultValue:Dynamic) {
+		function addOption(name:String, defaultValue:Dynamic)
 			replacements.push(TemplateUtils.addOption(name, "", defaultValue));
-		};
 
 		addOption("AUTHOR", settings.AuthorName);
 

src/commands/ConfigureCommand.hx
@@ -21,16 +21,7 @@ class ConfigureCommand extends Command
 			// TOOD: support other IDEs (need template replacements)
 			error('configure only supports ${IDE.VISUAL_STUDIO_CODE} at the moment');
 		
-		var projects = [];
-		var isProjectFolder = ProjectUtils.findProjectXml(directory) != null;
-		if (isProjectFolder)
-			projects.push(directory);
-		else
-		{
-			projects = ProjectUtils.findLimeProjects(directory).map(
-				function(p) return p.path
-			);
-		}
+		var projects = ProjectUtils.findLimeProjects(directory);
 
 		if (projects.length == 0)
 		{
@@ -40,9 +31,18 @@ class ConfigureCommand extends Command
 			
 		for (project in projects)
 		{
-			var fullPath = FileSystem.fullPath(project);
+			var fullPath = FileSystem.fullPath(project.path);
 			print('Adding $ide files to \'$fullPath\'...');
-			ProjectUtils.copyIDETemplateFiles(fullPath, null, ide);
+
+			var applicationName =
+				try
+					ProjectUtils.getApplicationFile(FileSystem.fullPath(project.projectXmlPath))
+				catch (_:Dynamic)
+					project.name;
+
+			var replacements = [TemplateUtils.addOption("APPLICATION_FILE", "", applicationName)];
+			replacements = ProjectUtils.copyIDETemplateFiles(fullPath, replacements, ide);
+			TemplateUtils.modifyTemplate(fullPath, replacements);
 		}
 	}
 }

src/utils/TemplateUtils.hx
@@ -182,9 +182,6 @@ typedef TemplateProject = {
 	Template:TemplateFile
 }
 
-/**
-* Definition of template replacement
-*/
 typedef TemplateReplacement = {
 	replacement:String,
 	pattern:String,

commit 535e96ff3ea36996374013ba92f7661e6b20b350

Fix "name" of ProjectUtils.findLimeProjects() results
(broken since 0e8c67d)

src/utils/ProjectUtils.hx
@@ -44,7 +44,7 @@ class ProjectUtils
 		if (projectPath != null)
 		{
 			projects.push({
-				name : new Path(projectPath).file,
+				name : new Path(directory).file,
 				path : directory,
 				projectXmlPath : projectPath,
 				targets : ""